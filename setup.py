31a0c58befbfb096525a6525484e7ccab5d5119f
kasahart <beinvoked66@gmail.com>
2025-02-18 14:16:17 +0000

    開発者インストール方法の変更

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="wandas",
-    version="0.1.0",
-    description="Signal processing library in Python, inspired by pandas",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/wandas",
-    license="MIT",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "librosa",
-        "mosqito",
-        "ipywidgets",
-        # Add other dependencies
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-    ],
-)
+setup()
